e23d8dac222586fcc25d0e4d16d9ac10e8be9843
devgemmy <anthonia@nugitech.com>
2025-03-04 16:30:56 +0000

    testimonials

--- src/constants/index.js
+++ src/constants/index.js
@@ -32,6 +32,7 @@ export const navLinks = [
   { id: "about", title: "About" },
   { id: "work", title: "Work" },
   { id: "projects", title: "Projects" },
+  { id: "testimonials", title: "Testimonials" },
   { id: "contact", title: "Contact" },
 ];
 
